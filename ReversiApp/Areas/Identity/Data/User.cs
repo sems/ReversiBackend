ec7973941c520092533be8e1124cc109ee6a9a4f
Sem Spanhaak <me@sems.dev>
2020-03-20 13:15:52 +0100

    Added token to User

--- ReversiApp/Areas/Identity/Data/User.cs
+++ ReversiApp/Areas/Identity/Data/User.cs
@@ -9,5 +9,6 @@ namespace ReversiApp.Areas.Identity.Data
     // Add profile data for application users by adding properties to the User class
     public class User : IdentityUser
     {
+        public string Token { get; set; }
     }
 }
